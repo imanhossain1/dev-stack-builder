8ff9f3b3b472569f668a877ea8781123ccb0fca2
imranhossain1 <imranhossainwork1@gmail.com>
2026-09-12 11:48:18 +0600

    feat: add stack remove and remove all functionality

--- src/components/YourStack.jsx
+++ src/components/YourStack.jsx
@@ -2,47 +2,68 @@ import React from "react";
 import {toast} from "react-toastify";
 
 function YourStack({selectedTechnologies, setSelectedTechnologies}) {
+    // single item delete
     const handleRemoveTechnology = (clickTechnologyId) => {
-        console.log(clickTechnologyId);
-
-        const updatedTechnologies  = selectedTechnologies.filter(
-            (technoLogy) => technoLogy.id !== clickTechnologyId
-        );
+        const updatedTechnologies = selectedTechnologies.filter((technoLogy) => technoLogy.id !== clickTechnologyId);
         setSelectedTechnologies(updatedTechnologies);
-        toast.error("Technology removed from your stack!");
+        toast.success("Removed from stack!");
+    };
+
+    //  seltected Technologies remove all
+    const removeAllTechnologies = () => {
+        setSelectedTechnologies([]);
+        toast.success("All removed!");
     };
 
     return (
         <aside className="card bg-base-100 border shadow-md">
             <div className="card-body">
                 {/* Heading */}
-                <div className="flex justify-between items-center">
+                <div className="">
                     <h2 className="text-xl font-bold">Your Stack</h2>
 
-                    <span className="badge badge-primary">{selectedTechnologies.length} Selected</span>
+                    <p className="badge  mt-3">{selectedTechnologies.length} Technologies Selected</p>
                 </div>
 
                 {/* Selected technologies */}
-                <div className="mt-4 space-y-3">
-                    {selectedTechnologies.map((technology) => (
-                        <div key={technology.id} className="flex items-center gap-3 border rounded-lg p-3">
-                            {/* Icon */}
-                            <img src={technology.icon} alt={technology.name} className="w-10 h-10 object-contain" />
+                {selectedTechnologies.length === 0 ? (
+                    <p className="text-center opacity-60 mt-3">No technologies selected yet.</p>
+                ) : (
+                    <>
+                        <div className="mt-4 space-y-3">
+                            {selectedTechnologies.map((technology) => (
+                                <div key={technology.id} className="flex items-center gap-3 border rounded-lg p-3">
+                                    {/* Icon */}
+                                    <img
+                                        src={technology.icon}
+                                        alt={technology.name}
+                                        className="w-10 h-10 object-contain"
+                                    />
 
-                            {/* Name + Category */}
-                            <div className="flex-1">
-                                <h3 className="font-semibold">{technology.name}</h3>
+                                    {/* Name + Category */}
+                                    <div className="flex-1">
+                                        <h3 className="font-semibold">{technology.name}</h3>
 
-                                <p className="text-sm opacity-60">{technology.category}</p>
-                            </div>
+                                        <p className="text-sm opacity-60">{technology.category}</p>
+                                    </div>
 
-                            {/* Remove button */}
-                            <button onClick={() => handleRemoveTechnology(technology.id)} className="btn btn-sm">
-                                ✕
+                                    {/* Remove button */}
+                                    <button
+                                        onClick={() => handleRemoveTechnology(technology.id)}
+                                        className="btn btn-sm text-2xl text-red-500 "
+                                    >
+                                        ✕
+                                    </button>
+                                </div>
+                            ))}
+                        </div>
+                        <div className="flex flex-col  mt-4 ">
+                            <button onClick={removeAllTechnologies} className="btn btn-outline btn-secondary w-full">
+                                Remove All
                             </button>
                         </div>
-                    ))}
-                </div>
+                    </>
+                )}
             </div>
         </aside>
     );
